eaafdb1c5b46e502dd130d3c4fddbb3dae88d9ce
Sharvari-19 <145306132+Sharvari-19@users.noreply.github.com>
2023-10-03 22:22:25 +0530

    Delete email.py

--- email.py
+++
@@ -1,9 +0,0 @@
-name=input("Enter Name:")
-email=input("Enter Email:")
-password=input("Enter Password:")
-if not name:
-    print("Error Name cannot be empty")
-    if not '@'in email:
-        print("Error Enter a valid email,@ missing")
-        if len(password)<8:
-            print("Error Password length less than 8 characters")
