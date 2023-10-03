e61a10fbca5137507386268c8a4841b04181cc75
Sharvari-19 <145306132+Sharvari-19@users.noreply.github.com>
2023-10-03 22:21:26 +0530

    email.py

--- email.py
+++ email.py
@@ -1,13 +1,9 @@
-name=input("Enter your name: ")
-email=input("Enter your email: ")
-password=input("Enter your password: ")
+name=input("Enter Name:")
+email=input("Enter Email:")
+password=input("Enter Password:")
 if not name:
-        print("Error Name cannot be empty.")
-if '@' not in email:
-        print("Error Missing '@' symbol.")
-
-if len(password) < 8:
-        print("Error: Password length must be at least 8 characters.")
-   print("Name:", name)
-    print("Email:", email)
-    print("Password:", password)
+    print("Error Name cannot be empty")
+    if not '@'in email:
+        print("Error Enter a valid email,@ missing")
+        if len(password)<8:
+            print("Error Password length less than 8 characters")
